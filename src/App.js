d8625dd4f8f22b98e05d8434038a2476fb337507
Konstantin.Kolesnik <kostya_kol333@mail.ru>
2023-01-19 09:07:48 +0300

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './scss/app.scss'
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
+import {Route, Routes} from "react-router-dom";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <div className='flex flex-col items-center'>
         <div className='container'>
             <Header />
-            <Main />
+            <>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </>
         </div>
     </div>
   );
